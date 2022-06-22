4c41a7e34aa53cd4f5e478a6557236d970299810
Freely Chrystal <fchrysta@student.21-school.ru>
2022-06-22 20:31:36 +0300

    refactor eat num checker and waitpid

--- philo_bonus/src/process_helpers.c
+++ philo_bonus/src/process_helpers.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   thread_helpers.c                                   :+:      :+:    :+:   */
+/*   process_helpers.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fchrysta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 11:47:12 by fchrysta          #+#    #+#             */
-/*   Updated: 2022/06/09 21:55:38 by fchrysta         ###   ########.fr       */
+/*   Updated: 2022/06/22 20:28:03 by fchrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,6 @@ int	check_philo_eat_num(t_vars *vars)
 	int	i;
 
 	i = 0;
-	if (vars->eat_num < 1)
-		exit(1);
 	while (i < vars->philo_num)
 	{
 		sem_wait(vars->eat_num_sem);
@@ -79,22 +77,17 @@ int	start_philosophers(t_vars *vars)
 
 int	start_processes(t_vars *vars)
 {
-	int	status;
-
-	vars->eat_num_check_pid = fork();
-	if (vars->eat_num_check_pid == -1)
-		return (8);
-	if (vars->eat_num_check_pid == 0)
-		check_philo_eat_num(vars);
+	if (vars->eat_num)
+	{
+		vars->eat_num_check_pid = fork();
+		if (vars->eat_num_check_pid == -1)
+			return (8);
+		if (vars->eat_num_check_pid == 0)
+			check_philo_eat_num(vars);
+	}
 	if (start_philosophers(vars))
 		return (9);
-	while (waitpid(-1, &status, 0) > 0)
-	{
-		if (WEXITSTATUS(status) == 0)
-		{
-			killer(vars);
-			return (0);
-		}
-	}
+	waitpid(-1, NULL, 0);
+	killer(vars);
 	return (0);
 }
